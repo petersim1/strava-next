00fcda037b68a02a7ebf0f10dad50e802339ce63
Peter Simone <plssimone@gmail.com>
2023-08-27 21:42:03 -0400

    update getURL to only return hostname

--- components/assets/connect.tsx
+++ components/assets/connect.tsx
@@ -7,13 +7,13 @@ const getURL = (): string => {
   const header = headers();
   return `${header.get("x-forwarded-proto") || (header.get("https") && "https") || "http"}://${
     header.get("x-forwarded-host") || header.get("host")
-  }/${header.get("x-invoke-path")}`;
+  }`;
 };
 
 export default (): JSX.Element => {
   // const CALLBACK = "http://localhost:3000/api/auth/callback";
   const url = getURL();
-  const CALLBACK = url + "api/auth/callback";
+  const CALLBACK = url + "/api/auth/callback";
 
   const URL = `https://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${CALLBACK}&response_type=code&approval_prompt=force&scope=activity:read`;
 

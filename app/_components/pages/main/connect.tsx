7bb5548107a06ee793d21a0cfec35fcbcac68612
Peter Simone <plssimone@gmail.com>
2023-10-31 16:16:17 -0400

    add oauth for mobile, default web fallback

--- app/_components/pages/main/connect.tsx
+++ app/_components/pages/main/connect.tsx
@@ -1,3 +1,5 @@
+import { headers } from "next/headers";
+
 import styles from "../styled.module.css";
 import { Connect } from "@/_components/assets";
 
@@ -5,10 +7,18 @@ export default (): JSX.Element => {
   // can uncomment below if I decide to go back to middleware instead of server action.
   // const athleteStr = headers().get("X-Strava-Athlete") ?? "{}";
   // const athlete: StravaAthleteI = JSON.parse(athleteStr);
+  const getURL = (): string => {
+    const header = headers();
+    return `${header.get("x-forwarded-proto") || (header.get("https") && "https") || "http"}://${
+      header.get("x-forwarded-host") || header.get("host")
+    }`;
+  };
+
+  const url = getURL();
   return (
     <div className={styles.connect_holder}>
       <p>{"Let's link your Strava so we can get started"}</p>
-      <Connect />
+      <Connect url={url} />
     </div>
   );
 };

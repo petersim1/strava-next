d397470275c3be08049ca9adc307ec62ae2a79ec
Peter Simone <plssimone@gmail.com>
2023-10-31 16:34:20 -0400

    remove logs

--- app/_components/assets/connect.tsx
+++ app/_components/assets/connect.tsx
@@ -6,7 +6,6 @@ const CALLBACK = "/api/auth/callback";
 
 export default ({ url }: { url: string }): JSX.Element => {
   // localhost is whitelisted.
-  // const urlUse = new URL("https://www.strava.com/oauth/mobile/authorize");
   const search = new URLSearchParams({
     client_id: process.env.NEXT_PUBLIC_CLIENT_ID || "",
     redirect_uri: url + CALLBACK,
@@ -14,12 +13,14 @@ export default ({ url }: { url: string }): JSX.Element => {
     approval_prompt: "force",
     scope: "activity:read",
   });
-  // will require patch, should accept a URLSearchParams object.
   let urlUse = new URL("strava://oauth/mobile/authorize");
+
+  // will require patch, should accept a URLSearchParams object.
   urlUse.search = search.toString();
   const onClick = (): void => {
     const now = new Date().valueOf();
     setTimeout(function () {
+      // navigating back to page still flashes web auth. Likely need to fix this.
       if (new Date().valueOf() - now > 100) return;
       urlUse = new URL("https://www.strava.com/oauth/mobile/authorize");
       urlUse.search = search.toString();

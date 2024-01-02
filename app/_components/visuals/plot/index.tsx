33ce5acfd840cde0a633af3969188f3ec382f1f3
Peter Simone <plssimone@gmail.com>
2024-01-02 16:11:12 -0500

    add mock modal with preview of content

--- app/_components/visuals/plot/index.tsx
+++ app/_components/visuals/plot/index.tsx
@@ -17,27 +17,30 @@ export default ({ data, opacity }: { data: PlotDataI[]; opacity: number }): JSX.
 
   useEffect(() => {
     const svg = select(ref.current);
+    const modal = select(modalRef.current);
 
+    modal.selectAll("*").remove();
     svg.selectAll("*").remove(); // remove current SVG so we can start fresh each run.
     if (data.length > 0) {
       createViz(svg, data, opacity);
     }
   }, [data, opacity]);
 
-  // const handleClick = (): void => {
-  //   if (!ref.current) return;
-  //   console.log(getMiles(data.reduce((a, b) => b.distance + a, 0)));
-  //   console.log(getHour(data.reduce((a, b) => b.moving_time + a, 0)));
-  //   console.log(getMinute(data.reduce((a, b) => b.moving_time + a, 0)));
-  //   html2canvas(ref.current).then((canvas) => {
-  //     const link = document.createElement("a");
-  //     link.download = "test.png";
-  //     link.href = canvas.toDataURL("image/png");
-  //     link.click();
-  //   });
-  // };
+  const handleDownload = (): void => {
+    const el = document.getElementById("modal-preview");
+    if (!el) return;
+    html2canvas(el, {
+      height: 500,
+      width: 500,
+    }).then((canvas) => {
+      const link = document.createElement("a");
+      link.download = "test.png";
+      link.href = canvas.toDataURL("image/png");
+      link.click();
+    });
+  };
 
-  const handleClick = (): void => {
+  const handleOpen = (): void => {
     const el = document.getElementById("modal-holder");
     if (!el) return;
     const svg = select(modalRef.current);
@@ -57,27 +60,36 @@ export default ({ data, opacity }: { data: PlotDataI[]; opacity: number }): JSX.
     <>
       <div className={styles.plot} ref={ref} />
       <div style={{ marginLeft: "auto", zIndex: 999 }}>
-        <button onClick={handleClick}>test</button>
+        <button onClick={handleOpen}>test</button>
       </div>
-      {ref.current && (
-        <div id="modal-holder" className={styles.modal_wrapper}>
-          <div onClick={handleClose}>X</div>
-          <div id="modal-preview" className={styles.modal_preview} ref={modalRef}>
-            <div style={{ position: "absolute", left: 0, top: 0 }}>
-              {data[0].sport_type === "Run" ? (
-                <Running fill="currentColor" height="1rem" width="1rem" />
-              ) : (
-                <Cycling fill="currentColor" height="1rem" width="1rem" />
-              )}
+      <div id="modal-holder" className={styles.modal_wrapper}>
+        <div className={styles.modal_container}>
+          <div className={styles.modal_header}>
+            <button onClick={handleDownload}>Download!</button>
+            <div onClick={handleClose} style={{ fontSize: "1.2rem", cursor: "pointer" }}>
+              X
+            </div>
+          </div>
+          <div id="modal-preview" className={styles.modal_preview}>
+            <div className={styles.img_preview_stats}>
+              <div>
+                <strong>Total Stats:</strong>
+              </div>
               <div>{getMiles(data.reduce((a, b) => b.distance + a, 0))}mi</div>
               <div>
                 {`${getHour(data.reduce((a, b) => b.moving_time + a, 0))}hr 
                 ${getMinute(data.reduce((a, b) => b.moving_time + a, 0))}min`}
               </div>
+              {data.length > 0 && data[0].sport_type === "Run" ? (
+                <Running fill="currentColor" height="1.5rem" width="1.5rem" />
+              ) : (
+                <Cycling fill="currentColor" height="1.5rem" width="1.5rem" />
+              )}
             </div>
+            <div className={styles.plot} ref={modalRef} />
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
